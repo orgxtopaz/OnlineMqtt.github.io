2a96a817e3622045621e5dd96205828982632b9f
zigm <77489316+orgxtopaz@users.noreply.github.com>
2021-04-05 11:35:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("index.js");
+
+// var client  = mqtt.connect({ host:'test.mosquitto.org', port: 8081})
+// or
+
+
+
+  
+
+// var pub_button = document.getElementById('pub-buttonpublish');
+// var pub_input = document.getElementById('pub-inputtopic');
+// pub_button.addEventListener('click', () => {
+//   // console.log('clicked');
+//   // console.log(pub_input.value);
+//   client.publish('junrey/messages', pub_input.value)
+//   pub_input.value = "";
+// })
+
+
+
+
+
+
+
